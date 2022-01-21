e8135457a77c2b4bfb53c30f42c3af15fc31f7f9
Francesca Cartia <francescacartia@yahoo.it>
2022-01-21 11:40:55 +0100

    Rails simple Airbnb done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+4.times do
+  flat = Flat.create!({ name: Faker::App.name, address: Faker::Address.street_address, description: Faker::Quote.most_interesting_man_in_the_world, price_per_night: Faker::Number.decimal, number_of_guests: Faker::Number.digit})
+  puts "Created #{flat.name}"
+end
+puts 'Finished!'
